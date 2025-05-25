6ab71b4ad0e3e0f4bc4d3da9eb322b8616b9061d
Mwafy123 <omarmwafy2003@gmail.com>
2025-05-25 17:01:52 +0300

    Local deployment added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
   forbidNonWhitelisted: true,
   transform: true,
 }));
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for my NestJS project')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // <- Accessible at /api-docs
+
+  await app.listen(process.env.PORT || 3000);
   await app.listen(3000);
 }
 bootstrap();
